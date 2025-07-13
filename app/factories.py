be3673056b8a56566493ca58b21ca5e1fa8f2d14
SSegey377 <147119853+SSegey377@users.noreply.github.com>
2025-07-13 17:30:01 +0900

    Update factories.py

--- app/factories.py
+++ app/factories.py
@@ -1,23 +1,20 @@
 import factory
 from factory import Faker, LazyAttribute
 from .models import Client, Parking
-
-
 class ClientFactory(factory.Factory):
     class Meta:
         model = Client
-
     name = Faker('first_name')
     surname = Faker('last_name')
     credit_card = LazyAttribute(
-        lambda x: None if factory.Faker('boolean').generate() else factory.Faker('credit_card_number').generate())
+        lambda x: None if Faker('boolean')() else Faker('credit_card_number')()
+    )
     car_number = Faker('license_plate')
 
 
 class ParkingFactory(factory.Factory):
     class Meta:
         model = Parking
-
     address = Faker('address')
     opened = Faker('boolean')
     count_places = Faker('random_int', min=1, max=100)
