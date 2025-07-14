807aea635938d67739ff12ec3956760fb993e683
SSegey377 <147119853+SSegey377@users.noreply.github.com>
2025-07-14 14:31:41 +0900

    upgrate15_fast_api_14_07

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ db = SQLAlchemy()
 
 
 class Client(db.Model):
-    __tablename__ = 'client'
+    __tablename__ = "client"
 
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     name = db.Column(db.String(50), nullable=False)
@@ -13,11 +13,11 @@ class Client(db.Model):
     car_number = db.Column(db.String(10), nullable=True)
 
     def __repr__(self):
-        return f'<Client {self.name} {self.surname}>'
+        return f"<Client {self.name} {self.surname}>"
 
 
 class Parking(db.Model):
-    __tablename__ = 'parking'
+    __tablename__ = "parking"
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     address = db.Column(db.String(100), nullable=False)
     opened = db.Column(db.Boolean, nullable=True)
@@ -25,20 +25,28 @@ class Parking(db.Model):
     count_available_places = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
-        return f'<Parking {self.address}>'
+        return f"<Parking {self.address}>"
 
 
 class ClientParking(db.Model):
-    __tablename__ = 'client_parking'
+    __tablename__ = "client_parking"
 
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-    client_id = db.Column(db.Integer, db.ForeignKey('client.id'), nullable=False)
-    parking_id = db.Column(db.Integer, db.ForeignKey('parking.id'), nullable=False)
+    client_id = db.Column(db.Integer, db.ForeignKey("client.id"),
+                          nullable=False)
+    parking_id = db.Column(db.Integer, db.ForeignKey("parking.id"),
+                           nullable=False)
     time_in = db.Column(db.DateTime, nullable=False)
     time_out = db.Column(db.DateTime, nullable=True)
-    client = db.relationship('Client', backref='client_parking')
-    parking = db.relationship('Parking', backref='client_parking')
-    __table_args__ = (db.UniqueConstraint('client_id', 'parking_id', name='unique_client_parking'),)
+    client = db.relationship("Client", backref="client_parking")
+    parking = db.relationship("Parking", backref="client_parking")
+    __table_args__ = (
+        db.UniqueConstraint("client_id", "parking_id",
+                            name="unique_client_parking"),
+    )
 
     def __repr__(self):
-        return f'<ClientParking ClientID: {self.client_id}, ParkingID: {self.parking_id}>'
+        return (
+            f"<ClientParking ClientID: {self.client_id}, "
+            f"ParkingID: {self.parking_id}>"
+        )
--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+
 from flask import Blueprint, jsonify, request
 from datetime import datetime
 from .models import db, Client, Parking, ClientParking
